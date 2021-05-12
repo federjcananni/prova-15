ff2cdd6f5cdd0b6eaf8fefd7e52a86a7bb64a0a5
federjcananni <82090961+federjcananni@users.noreply.github.com>
2021-05-12 12:31:23 +0200

    ip

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@
                     {
                         scrollTop: target.offset().top - 72,
                     },
-                    500,
+                    499,
                     "easeInOutExpo"
                 );
                 return false;
